47397e33ee80fa4455975d46d6d34718d9a58a56
Rekha Kumar <rekhakumar50@gmail.com>
2025-01-22 17:36:39 +0800

    enum changes

---
+++ src/main/java/com/example/demo/Enum/EntityTypeEnum.java
@@ -0,0 +1,48 @@
+package com.example.demo.Enum;
+
+import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+
+@Getter
+public enum EntityTypeEnum {
+
+    SOLE_PROPRIETOR("Sole Proprietor", "PP"),
+    PARTNERSHIP("Partnership", "PP"),
+    LIMITED_LIABILITY_PARTNERSHIP("Limited Liability Partnership", "LLP"),
+    PRIVATE_LIMITED_COMPANY("Private Limited Company", "PRLTD"),
+    EXEMPT_PRIVATE_COMPANY_LIMITED_BY_SHARES("EXEMPT PRIVATE COMPANY LIMITED BY SHARES", "PRLTD"),
+    PUBLIC_COMPANY("Public Company", "PUB"),
+    PUBLIC_COMPANY_LIMITED_BY_SHARES("PUBLIC COMPANY LIMITED BY SHARES", "PUB"),
+    PUBLIC_COMPANY_LIMITED_BY_GUARANTEE("PUBLIC COMPANY LIMITED BY GUARANTEE", "PUB"),
+    UNLIMITED_COMPANY("Unlimited Company", "UNLTD");
+
+    private final String type;
+    private final String code;
+
+    //By default, enum constructor is private. Other modifiers are not allowed
+    EntityTypeEnum(String type, String code) {
+        this.type = type;
+        this.code = code;
+    }
+
+    public static String getCodeForType(String type) {
+        return Arrays.stream(EntityTypeEnum.values())
+                .filter(entityType -> StringUtils.equalsIgnoreCase(entityType.type, type))
+                .map(EntityTypeEnum::getCode)
+                .findFirst()
+                .orElseGet(() -> {
+                    String result = "";
+                    if (StringUtils.containsIgnoreCase(type, "PRIVATE")) {
+                        result = "PRLTD";
+                    } else if (StringUtils.containsIgnoreCase(type, "PUBLIC")) {
+                        result = "PUB";
+                    } else if (StringUtils.containsIgnoreCase(type, "UNLIMITED")) {
+                        result = "UNLTD";
+                    }
+                    return result;
+                });
+    }
+
+}
---
+++ src/main/java/com/example/demo/Enum/Test.java
@@ -0,0 +1,7 @@
+package com.example.demo.Enum;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(EntityTypeEnum.getCodeForType("Private Company"));
+    }
+}
